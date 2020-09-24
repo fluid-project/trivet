6bad46733b583e948a542016b7b50b5266b14363
Ned Zimmerman <ned@bight.dev>
2020-09-24 14:57:23 -0300

    fix: adjust copyright holders

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*
-Copyright the Infusion copyright holders.
+Copyright the Fluidic Eleventy copyright holders.
 
 See the AUTHORS.md file at the top-level directory of this distribution and at
 https://github.com/fluid-project/fluidic-11ty/raw/master/AUTHORS.md.
