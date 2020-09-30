9aafd0d15e15179796e30ef1a4968595010c2f49
Ned Zimmerman <ned@bight.dev>
2020-09-30 15:53:57 -0300

    fix: add trailing newlines

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,19 @@
-const dev = require('./webpack.dev.js');
-const merge = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
+const dev = require("./webpack.dev.js");
+const merge = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
 
 // @see https://github.com/clenemt/eleventy-webpack/blob/master/webpack.prod.js
 
 module.exports = merge(dev, {
-  mode: 'production',
-  watch: false,
-  devtool: false,
-  output: { filename: '[name]-[hash].js' },
-  optimization: {
-	minimizer: [
-	  new TerserPlugin({
-		sourceMap: true
-	  })
-	]
-  }
+    mode: "production",
+    watch: false,
+    devtool: false,
+    output: { filename: "[name]-[hash].js" },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true
+            })
+        ]
+    }
 });
