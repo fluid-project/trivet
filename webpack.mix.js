db33cb29ffbd5b15559e02ad79058fd27b751af7
Ned Zimmerman <ned@bight.dev>
2020-10-14 18:07:29 -0300

    fix: remove manifest file from dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,42 @@
 "use strict";
 
 const mix = require("laravel-mix");
-const cpFile = require("cp-file");
+const moveFile = require("move-file");
 const fg = require("fast-glob");
 
+// Set public path.
 mix.setPublicPath("dist/assets");
 
 const scriptPaths = fg.sync("./src/assets/scripts/*.js");
 
+// Process JavaScript with Babel.
 scriptPaths.forEach(entryPath => {
     mix.js(entryPath, "dist/assets/scripts");
 });
 
+// Process CSS with PostCSS.
 mix.postCss("src/assets/styles/app.css", "dist/assets/styles");
+
+// Compile Sass.
 // mix.sass("src/assets/styles/app.scss", "dist/assets/styles");
+
+// Compile Stylus.
 // mix.stylus("src/assets/styles/app.styl", "dist/assets/styles");
 
-mix.then(() => {
-    cpFile.sync("dist/assets/mix-manifest.json", "src/_data/assets.json");
+mix.options({
+    // Don't modify stylesheet url() functions.
+    processCssUrls: false
 });
 
+// Enable source maps.
+mix.sourceMaps(false, "source-map");
+
+// Add version string to assets in production.
 if (mix.inProduction()) {
     mix.version();
 }
+
+// Copy asset manifest into Eleventy's data directory to trigger a build when assets are updated.
+mix.then(() => {
+    moveFile.sync("dist/assets/mix-manifest.json", "src/_data/assets.json");
+});
